8773c930036f608a0220efb1c973ee11cdff4235
thehappycheese <the.nicholas.archer@gmail.com>
2024-06-06 09:28:41 +0000

    add Cut and CutTwice to LineStringMeasured

--- LineStringTools.cs
+++ LineStringTools.cs
@@ -33,6 +33,84 @@ namespace LineStringTools
             TotalLength = lineString.Length;
         }
 
+          public (LineStringMeasured, LineStringMeasured) Cut(double fractionOfLength)
+        {
+            double distanceAlong = TotalLength * fractionOfLength;
+
+            if (distanceAlong <= 0)
+            {
+                return (null, this);
+            }
+            else if (distanceAlong >= TotalLength)
+            {
+                return (this, null);
+            }
+            else
+            {
+                double distanceSoFar = 0;
+                double distanceRemaining = distanceAlong;
+                for (int i = 0; i < Segments.Count; i++)
+                {
+                    var segment = Segments[i];
+                    if (distanceRemaining <= 0)
+                    {
+                        return (
+                            new LineStringMeasured(CreateLineString(Segments.GetRange(0, i))),
+                            new LineStringMeasured(CreateLineString(Segments.GetRange(i, Segments.Count - i)))
+                        );
+                    }
+                    else if (distanceRemaining < segment.Length)
+                    {
+                        double ratio = distanceRemaining / segment.Length;
+                        double x = segment.A.X + (segment.B.X - segment.A.X) * ratio;
+                        double y = segment.A.Y + (segment.B.Y - segment.A.Y) * ratio;
+                        var intermediatePoint = new Coordinate(x, y);
+
+                        var part1Segments = new List<LineSegmentMeasured>(Segments.GetRange(0, i))
+                        {
+                            new LineSegmentMeasured(segment.A, intermediatePoint)
+                        };
+
+                        var part2Segments = new List<LineSegmentMeasured>
+                        {
+                            new LineSegmentMeasured(intermediatePoint, segment.B)
+                        };
+                        part2Segments.AddRange(Segments.GetRange(i + 1, Segments.Count - i - 1));
+
+                        return (
+                            new LineStringMeasured(CreateLineString(part1Segments)),
+                            new LineStringMeasured(CreateLineString(part2Segments))
+                        );
+                    }
+                    distanceSoFar += segment.Length;
+                    distanceRemaining -= segment.Length;
+                }
+            }
+
+            return (null, null);
+        }
+
+        public (LineStringMeasured, LineStringMeasured, LineStringMeasured) CutTwice(double fractionOfLengthStart, double fractionOfLengthEnd)
+        {
+            var (a, bc) = Cut(fractionOfLengthStart);
+            if (bc == null)
+            {
+                return (a, null, null);
+            }
+            var (b, c) = bc.Cut((fractionOfLengthEnd - fractionOfLengthStart) / (1 - fractionOfLengthStart));
+            return (a, b, c);
+        }
+        private LineString CreateLineString(List<LineSegmentMeasured> segments)
+        {
+            var coordinates = new List<Coordinate>();
+            foreach (var segment in segments)
+            {
+                coordinates.Add(segment.A);
+            }
+            coordinates.Add(segments[segments.Count - 1].B);
+            return new LineString(coordinates.ToArray());
+        }
+
         public Coordinate? Interpolate(double fractionOfLength)
         {
             if (Segments.Count == 0) return null;
@@ -55,6 +133,20 @@ namespace LineStringTools
             }
 
             return Segments[Segments.Count - 1].B;
+        }
+        
+        public List<double[]> ToCoordinateList() {
+            var coordinates = new List<double[]>();
+            foreach (var segment in Segments)
+            {
+                coordinates.Add(new double[] { segment.A.X, segment.A.Y });
+            }
+            // Add the last point
+            if (Segments.Count > 0)
+            {
+                coordinates.Add(new double[] { Segments[Segments.Count - 1].B.X, Segments[Segments.Count - 1].B.Y });
+            }
+            return coordinates;
         }
     }
 }
